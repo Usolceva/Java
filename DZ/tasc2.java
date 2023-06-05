7e6dfa2236a79606e1c11ca969b932a48ad10e5d
olga <olga.matveeva.92@inbox.ru>
2023-06-05 14:42:01 +0800

    строка sql-запроса, пузырьковый метод, json строка

---
+++ DZ/tasc2.java
@@ -0,0 +1,106 @@
+// Дана строка sql-запроса "select * from students where ". Сформируйте часть WHERE этого запроса, 
+// используя StringBuilder. Данные для фильтрации приведены ниже в виде json строки.
+// Если значение null, то параметр не должен попадать в запрос.
+// Параметры для фильтрации: {"name":"Ivanov", "country":"Russia", "city":"Moscow", "age":"null"}
+public class tasc2 {
+    public static void main(String[] args) throws Exception {
+      String [] list = lib.ReadLineFromFile("dataForSelect.txt"); 
+      System.out.println(list[0]); 
+      StringBuilder resultSelect = LineInList(list[0]);
+      System.out.println(resultSelect);
+    }
+    public static StringBuilder LineInList(String line) {
+        String line1 = line.replace("{", "");
+        String line2 = line1.replace("}", "");
+        String line3 = line2.replaceAll("\"", "");
+        System.out.println(line3);
+        StringBuilder result = new StringBuilder("select * from students where ");
+
+        String [] arrayData = line3.split(", ");
+        for (int i =0; i < arrayData.length; i++) {
+            String[] arrData = arrayData[i].split(":");
+            if(arrData[1].equals("null") == false) {
+                if (i != 0) {
+                    result.append(", ");
+                    result.append(arrData[0]);
+                    result.append(" = ");
+                    result.append(arrData[1]);
+                } else {
+                    result.append(arrData[0]);
+                    result.append(" = ");
+                    result.append(arrData[1]);
+                }
+            }
+            
+        }
+        return result;
+    }
+}
+
+
+
+// Реализуйте алгоритм сортировки пузырьком числового массива, результат после каждой итерации запишите в лог-файл.
+import java.io.IOException;
+import java.util.logging.*;
+    public static void main(String[] args) throws IOException {
+        Logger logger = Logger.getLogger(task2.class.getName());
+        FileHandler fh = new FileHandler("logTask2.xml");
+        logger.addHandler(fh);
+        XMLFormatter xml = new XMLFormatter();
+        fh.setFormatter(xml);
+        logger.info("Sort Array");
+        int [] array = {2, 5, 1, -7, 3, 64, 0};
+        lib.printArrayInt(array);
+        int[] newArr = listSort(array);
+        System.out.printf("\n Отсортированный массив: \n");
+        lib.printArrayInt(newArr);
+    }
+    public static int[] listSort(int [] arr){
+        for(int k = 0; k < arr.length-1; k++) {
+            
+            for (int i = 0; i < arr.length - k-1; i++){
+                if (arr[i] > arr[i+1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i+1];
+                    arr[i+1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+
+/* Дана json строка (можно сохранить в файл и читать из файла)
+[{"фамилия":"Иванов","оценка":"5","предмет":"Математика"},{"фамилия":"Петрова","оценка":"4","предмет":"Информатика"},{"фамилия":"Краснов","оценка":"5","предмет":"Физика"}]
+Написать метод(ы), который распарсит json и, используя StringBuilder, создаст строки вида: Студент [фамилия] получил [оценка] по предмету [предмет].
+Пример вывода:
+Студент Иванов получил 5 по предмету Математика.
+Студент Петрова получил 4 по предмету Информатика.
+Студент Краснов получил 5 по предмету Физика. */
+
+
+    public static void main(String[] args) throws Exception {
+        String [] arrayData =  lib.ReadLineFromFile("dataForTask3.txt");
+        for(int i = 0; i < arrayData.length; i++) {
+            System.out.println(PrintLine(arrayData[i]));
+        }
+        
+    }
+    public static StringBuilder PrintLine(String line) {
+        String line1 = line.replace("{", "");
+        String line2 = line1.replace("}", "");
+        String line3 = line2.replaceAll("\"", "");
+        String line4 = line3.replace("[", "");
+        String line5 = line4.replace("]", "");
+        StringBuilder result = new StringBuilder("");
+        String [] arrayData = line5.split(",");
+        String [] listName = {"Студент ", " получил ", " по предмету "};
+        for (int i =0; i < arrayData.length; i++) {
+            String[] arrData = arrayData[i].split(":");
+            result.append(listName[i]);
+            result.append(arrData[1]);
+            }
+        return result;
+    }
+
+
